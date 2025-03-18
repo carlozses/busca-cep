b42b437ae250215889b0058db4c2368b474a99c8
Carloz Eduardo Rosa Jacoia <ra2357046>
2025-03-18 10:49:16 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [cep, setCep] = useState('');
+  const [endereco, setEndereco] = useState(
+    {
+      logradouro: '',
+      localidade:'',
+      bairro:'',
+      estado:'',
+      regiao:''
+    }
+  );
+
+  async function buscarCEP(){
+    let r = await fetch("https://viacep.com.br/ws/"+cep+"/json/");
+    let dados = await r.json();
+    setEndereco(dados);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Consulte seu CEP</Text>
+      <TextInput 
+      style={styles.textinput}
+      value={cep}
+      onChangeText={setCep}
+      />
+      <Text>{cep}</Text>
+      <Button title='Buscar'
+      onPress={buscarCEP}
+      />
+      <Text>{endereco.logradouro}</Text>
+      <Text>{endereco.localidade}</Text>
+      <Text>{endereco.bairro}</Text>
+      <Text>{endereco.estado}</Text>
+      <Text>{endereco.regiao}</Text>
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -17,4 +50,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textinput:{
+    borderWidth: 1,
+    color: 'red'
+  }
 });
